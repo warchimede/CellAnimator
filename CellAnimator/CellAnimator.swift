cc62c3a5fe1badde638ddefa812dbe8210555516
Alex Loren <lorenalexm@gmail.com>
2018-01-14 11:16:08 -0800

    More 'swifty'
    Made transform method calls private. Transform now selected via enum value in function call. Renamed animate function to follow more closely to modern Swift naming standards.

--- CellAnimator/CellAnimator.swift
+++ CellAnimator/CellAnimator.swift
@@ -12,7 +12,18 @@ import UIKit
 
 public class CellAnimator {
     
-    public static let TransformTipIn = { (layer: CALayer) -> CATransform3D in
+    public enum AnimationType: Int {
+        case TipIn = 0
+        case Curl
+        case Fan
+        case Flip
+        case Helix
+        case Tilt
+        case Wave
+        case count
+    }
+    
+    private static let transformTipIn = { (layer: CALayer) -> CATransform3D in
         let rotationDegrees: CGFloat = -15.0
         let rotationRadians: CGFloat = rotationDegrees * (CGFloat(Double.pi)/180.0)
         let offset = CGPoint(x: -20, y: -20)
@@ -23,7 +34,7 @@ public class CellAnimator {
         return transform
     }
     
-    public static let TransformCurl = { (layer: CALayer) -> CATransform3D in
+    private static let transformCurl = { (layer: CALayer) -> CATransform3D in
         var transform = CATransform3DIdentity
         transform.m34 = 1.0 / -500
         transform = CATransform3DTranslate(transform, -layer.bounds.size.width/2.0, 0.0, 0.0)
@@ -33,7 +44,7 @@ public class CellAnimator {
         return transform
     }
     
-    public static let TransformFan = { (layer: CALayer) -> CATransform3D in
+    private static let transformFan = { (layer: CALayer) -> CATransform3D in
         var transform = CATransform3DIdentity
         transform = CATransform3DTranslate(transform, -layer.bounds.size.width/2.0, 0.0, 0.0)
         transform = CATransform3DRotate(transform, -CGFloat(Double.pi)/2.0, 0.0, 0.0, 1.0)
@@ -41,7 +52,7 @@ public class CellAnimator {
         return transform
     }
     
-    public static let TransformFlip = { (layer: CALayer) -> CATransform3D in
+    private static let transformFlip = { (layer: CALayer) -> CATransform3D in
         var transform = CATransform3DIdentity
         transform = CATransform3DTranslate(transform, 0.0, layer.bounds.size.height/2.0, 0.0)
         transform = CATransform3DRotate(transform, CGFloat(Double.pi)/2.0, 1.0, 0.0, 0.0)
@@ -49,7 +60,7 @@ public class CellAnimator {
         return transform
     }
     
-    public static let TransformHelix = { (layer: CALayer) -> CATransform3D in
+    private static let transformHelix = { (layer: CALayer) -> CATransform3D in
         var transform = CATransform3DIdentity
         transform = CATransform3DTranslate(transform, 0.0, layer.bounds.size.height/2.0, 0.0)
         transform = CATransform3DRotate(transform, CGFloat(Double.pi), 0.0, 1.0, 0.0)
@@ -57,27 +68,44 @@ public class CellAnimator {
         return transform
     }
     
-    public static let TransformTilt = { (layer: CALayer) -> CATransform3D in
+    private static let transformTilt = { (layer: CALayer) -> CATransform3D in
         var transform = CATransform3DIdentity
         transform = CATransform3DScale(transform, 0.8, 0.8, 0.8)
         return transform
     }
     
-    public static let TransformWave = { (layer: CALayer) -> CATransform3D in
+    private static let transformWave = { (layer: CALayer) -> CATransform3D in
         var transform = CATransform3DIdentity
         transform = CATransform3DTranslate(transform, -layer.bounds.size.width/2.0, 0.0, 0.0)
         return transform
     }
     
-    public class func animateCell(cell: UITableViewCell, withTransform transform: (CALayer) -> CATransform3D, andDuration duration: NSTimeInterval) {
-        
-        let view = cell.contentView    
-        view.layer.transform = transform(cell.layer)
+    public class func animate(_ cell: UITableViewCell, withDuration duration: TimeInterval, animation type: AnimationType) {
+        let view = cell.contentView
         view.layer.opacity = 0.8
-            
-        UIView.animateWithDuration(duration) {
+        
+        switch type {
+        case .TipIn:
+            view.layer.transform = transformTipIn(cell.layer)
+        case .Curl:
+            view.layer.transform = transformCurl(cell.layer)
+        case .Fan:
+            view.layer.transform = transformFan(cell.layer)
+        case .Flip:
+            view.layer.transform = transformFlip(cell.layer)
+        case .Helix:
+            view.layer.transform = transformHelix(cell.layer)
+        case .Tilt:
+            view.layer.transform = transformTilt(cell.layer)
+        case .Wave:
+            view.layer.transform = transformWave(cell.layer)
+        default:
+            view.layer.transform = transformTipIn(cell.layer)
+        }
+        
+        UIView.animate(withDuration: duration, animations: {
             view.layer.transform = CATransform3DIdentity
             view.layer.opacity = 1
-        }
+        })
     }
 }
